8340d2f30e0be1a4d8e88b9cd269d9faf8d7080b
Plat <60182057+p1atdev@users.noreply.github.com>
2024-08-16 01:43:39 +0900

    chore: add test of loading config

--- src/config.rs
+++ src/config.rs
@@ -10,32 +10,14 @@ pub struct ModelConfig {
     pub architecture: String,
     pub num_classes: u32,
     pub num_features: u32,
-    pub global_pool: String,
-    pub model_args: ModelArgs,
     pub pretrained_cfg: PretrainedCfg,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModelArgs {
-    pub act_layer: String,
-    pub img_size: u32,
-    pub window_size: u32,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PretrainedCfg {
-    pub custom_load: bool,
-    pub input_size: Vec<u32>,
+    pub input_size: Vec<u32>, // [channels, height, width]
     pub fixed_input_size: bool,
-    pub interpolation: String,
-    pub crop_pct: u32,
-    pub crop_mode: String,
-    pub mean: Vec<f64>,
-    pub std: Vec<f64>,
     pub num_classes: u32,
-    pub pool_size: Option<String>,
-    pub first_conv: Option<String>,
-    pub classifier: Option<String>,
 }
 
 impl ModelConfig {
@@ -45,5 +27,43 @@ impl ModelConfig {
         let config: ModelConfig =
             serde_json::from_str(&json).map_err(|e| TaggerError::Io(e.to_string()))?;
         Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::file::ConfigFile;
+    use std::fs;
+
+    #[test]
+    fn test_load_model_config() {
+        let config_file = ConfigFile::new("SmilingWolf/wd-swinv2-tagger-v3")
+            .get()
+            .unwrap();
+        let json = fs::read_to_string(config_file).unwrap();
+        let _config: ModelConfig = serde_json::from_str(&json).unwrap();
+    }
+
+    #[test]
+    fn test_load_model_config_from_pretrained() {
+        let _config = ModelConfig::from_pretrained("SmilingWolf/wd-swinv2-tagger-v3").unwrap();
+    }
+
+    #[test]
+    fn test_load_model_config_from_pretrained_many() {
+        let repo_ids = vec![
+            "SmilingWolf/wd-eva02-large-tagger-v3".to_string(),
+            "SmilingWolf/wd-vit-large-tagger-v3".to_string(),
+            "SmilingWolf/wd-v1-4-swinv2-tagger-v2".to_string(),
+            "SmilingWolf/wd-vit-tagger-v3".to_string(),
+            "SmilingWolf/wd-swinv2-tagger-v3".to_string(),
+            "SmilingWolf/wd-convnext-tagger-v3".to_string(),
+        ];
+
+        for repo_id in repo_ids {
+            let _config = ModelConfig::from_pretrained(&repo_id);
+            assert!(_config.is_ok(), "{}", repo_id);
+        }
     }
 }
